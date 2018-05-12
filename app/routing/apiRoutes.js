fafd6b9c3c8bbd22f22673abee81762aef135571
Matt Brzowski <5thhand@gmail.com>
2018-05-11 20:54:11 -0600

    adjust main functionality and prep for deployment

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,13 @@
 const path = require("path");
+const fs = require("fs");
+
+const friendsArray = require("../data/friends.js");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
@@ -23,11 +26,32 @@ module.exports = function(app) {
   //   res.sendFile(path.join(__dirname, "../public/reserve.html"));
   // });
 
-  app.get('/api/friends', function(req, res) {
-    res.send(JSON.stringify(path.join(__dirname, "../public/data/friends.js")));
+  app.get('/api/friends', function (req, res) {
+    res.sendFile(path.join(__dirname, "../data/friends.js"));
   });
-  
-  app.post('/api/friends', function(req, res) {
 
+  app.post('/api/friends', function (req, res) {
+    var newFriend = req.body;
+    var reducer = (accumulator, currentValue) => accumulator + currentValue;
+    var current = 50;
+    var index = 0;
+
+    for (var i = 0; i < friendsArray.length; i++) {
+      var test = newFriend.scores.reduce(reducer) - friendsArray[i].scores.reduce(reducer);
+      if (test < 0) {
+        test = test * -1;
+      }
+      if (test < current) {
+        current = test;
+        index = i;
+      }
+    }
+
+    var bestFriend = friendsArray[index];
+    res.send(JSON.stringify(bestFriend));
+    friendsArray.push(req.body);
+    fs.writeFile('./app/data/friends.js', `const friendsArray = ${JSON.stringify(friendsArray)}; \nmodule.exports = friendsArray`, (err) => {
+      if (err) throw err;
+    });
   });
 };
